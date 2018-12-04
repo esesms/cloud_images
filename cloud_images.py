769fed89293c18d9577bd63eb044ddcad3fff001
Sue <huangs@ucla.edu>
2018-12-03 21:45:36 -0500

    Tidied up the photo search parameters

--- cloud_images.py
+++ cloud_images.py
@@ -3,29 +3,34 @@ import urllib.request
 #from PIL import Image
 
 
-api_key = u'8ae389de9dbb32ad931ae7745672484f'
-api_secret = u'd6454322bc20a4b5'
+api_key = '8ae389de9dbb32ad931ae7745672484f'
+api_secret = 'd6454322bc20a4b5'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
-keyword = 'clouds'
+#keyword = 'clouds'
 
 urls = []
 
-for photo in flickr.walk(tag_mode='all',
-        tags = 'cloud, sky',
-        extras = 'url_c',
-        sort = 'relevance',
-        min_upload_date = '1543510800',
-        max_upload_date = '1543597200',
+ix = 0
+for photo in flickr.walk(
+        tags = 'cloud, clouds, sky, -plane, -airplane',
+        tag_mode='all',    #all for AND, any for OR
+        min_taken_date = '2018-11-01 12:00:00',
+        max_taken_date = '2018-11-30 12:00:00',
+        sort = 'relevance',    
+        safe_search = 1,
         content_type = 1,
+        extras = 'url_c',
         per_page = 10,
-        color_codes = 'c'):    #7 for cyan, 8 for blue, c for white
-    title = photo.get('title') #get title
-    url = photo.get('url_c')   #get url
-    urls.append(url)           #add url to the urls array
-    print(title)
-    print(url)
+        color_codes = '8'):     #7 for cyan, 8 for blue, c for white
+    title = photo.get('title')  #get title
+    url = photo.get('url_c')    #get url
+    urls.append(url)            #add url to the urls array
+    print(title)                #print the title of the image               
+    print(url)                  #print the url of the image
+    print(ix)                   #print the index
+    ix += 1
     #i = len(urls)-1
     #print(i)
   
@@ -33,8 +38,9 @@ for photo in flickr.walk(tag_mode='all',
 
 for index,s in enumerate(urls):
     i = index
-    print(i)
-    urllib.request.urlretrieve(urls[i], str(i) + '.jpg')
+    print(i)                    #print the index
+    if urls[i]:
+        urllib.request.urlretrieve(urls[i], str(i) + '.jpg')
     
 
 
