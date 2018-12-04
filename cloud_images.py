0ff8e5490080742317400ee68d541c117cb823e6
Sue <huangs@ucla.edu>
2018-12-03 21:48:14 -0500

    Cleaned up

--- cloud_images.py
+++ cloud_images.py
@@ -1,18 +1,15 @@
 import flickrapi
 import urllib.request
-#from PIL import Image
-
 
 api_key = '8ae389de9dbb32ad931ae7745672484f'
 api_secret = 'd6454322bc20a4b5'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
-#keyword = 'clouds'
-
 urls = []
 
 ix = 0
+
 for photo in flickr.walk(
         tags = 'cloud, clouds, sky, -plane, -airplane',
         tag_mode='all',    #all for AND, any for OR
@@ -31,20 +28,12 @@ for photo in flickr.walk(
     print(url)                  #print the url of the image
     print(ix)                   #print the index
     ix += 1
-    #i = len(urls)-1
-    #print(i)
   
-    #Download image from the url and save it to '00001.jpg'
+    
 
 for index,s in enumerate(urls):
     i = index
     print(i)                    #print the index
     if urls[i]:
-        urllib.request.urlretrieve(urls[i], str(i) + '.jpg')
-    
-
-
-#local_filename, headers = urllib.request.urlretrieve(urls[10])
-#html = open(local_filename)
-    
+        urllib.request.urlretrieve(urls[i], str(i) + '.jpg')    #Download image from the url and save it to '00001.jpg'
     
